bf027686acf32a760e93eb8c2edbf96e49d0dd85
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-15 12:09:52 +0200

    style/code

---
+++ lib/style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static final Color backgroundColor = Color(0xFF090B22);
+  static final Color activeCardColor = Color(0xFF1D1F33);
+  static final Color inactiveCardColor = Color(0xFF101427);
+  static final Color accentColor = Color(0xFFEA1555);
+  static final Color _secondaryColor = Color(0xFF8D8E98);
+
+  static  Color maleCardColor = inactiveCardColor;
+  static  Color femaleCardColor = inactiveCardColor;
+
+  static const bottomContainerHeight = 80.0;
+  static const _iconSize = 80.0;
+  static const _defaultFontSize = 18.0;
+
+  static const _margin = 15.0;
+
+  static final textSecondary = TextStyle(
+    fontSize: _defaultFontSize,
+    color: _secondaryColor,
+  );
+
+  static defaultIconSize() {
+    return _iconSize;
+  }
+
+  static defaultMargin() {
+    return _margin;
+  }
+}
