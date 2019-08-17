d58b3b18d1c46e55a107942bbb8270e998983657
Rishabh Dev <rishabhdev.work@gmail.com>
2019-08-17 12:11:48 +0200

    code/refactor

---
+++ lib/icon_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+class IconCard extends StatelessWidget {
+  final String gender;
+  final IconData icon;
+
+  IconCard({@required this.gender, @required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: Style.defaultIconSize(),
+        ),
+        SizedBox(
+          height: Style.defaultMargin(),
+        ),
+        Text(
+          gender,
+          style: Style.textSecondary,
+        ),
+      ],
+    );
+  }
+}
